248b1d2c61b122557a1d52252b6b4057f2a8390c
Fahad-Husxain <126326422+Fahad-Husxain@users.noreply.github.com>
2025-12-12 01:05:34 +0500

    Added Whitelist to Admin Panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import WhitelistDomain, ScanLog
 
-# Register your models here.
+# Ye code Django ko bolta hai ke in tables ko Admin Panel mein dikhao
+@admin.register(WhitelistDomain)
+class WhitelistDomainAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'rank')
+    search_fields = ('domain',)
+
+@admin.register(ScanLog)
+class ScanLogAdmin(admin.ModelAdmin):
+    list_display = ('url', 'status', 'confidence', 'timestamp', 'country')
+    list_filter = ('status', 'country')
