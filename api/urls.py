3c41ce2cd312622ea28886560517df84d5e1bc9b
Fahad-Husxain <126326422+Fahad-Husxain@users.noreply.github.com>
2026-05-12 04:06:25 +0500

    Fixed urls and moved backdoor to api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import analytics_view
 from .views import predict_url, dashboard_stats, search_whitelist, report_safe, fix_everything 
-from api.views import setup_admin_backdoor
 # 👆 'fix_everything' ko import mein add karna mat bhoolna
 
 urlpatterns = [
@@ -13,12 +12,4 @@ urlpatterns = [
     # 🔥 YE SECRET LINK HAI:
     path('fix-now/', fix_everything, name='fix_everything'),
     path('analytics/', analytics_view, name='analytics'),
-]
-# Import mein ye line add karo (agar api app ka naam hai):
-from api.views import setup_admin_backdoor 
-
-# urlpatterns ke andar ye line add karo:
-urlpatterns = [
-    # ... tumhare baaki links ...
-    path('setup-admin/', setup_admin_backdoor), 
 ]
